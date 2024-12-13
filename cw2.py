01741e4a5f19a022e03fa6f1c825578440c26c91
sox1d <yaskodanil@gmail.com>
2024-12-14 01:15:53 +0300

    First

---
+++ cw2.py
@@ -0,0 +1,89 @@
+import sys
+from random import choice
+import io
+
+import PyQt6
+from PyQt6 import uic
+from PyQt6.QtCore import QPointF
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton
+
+template = '''<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>MainWindow</class>
+ <widget class="QMainWindow" name="MainWindow">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>600</width>
+    <height>600</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>MainWindow</string>
+  </property>
+  <widget class="QWidget" name="centralwidget">
+   <widget class="QPushButton" name="button">
+    <property name="geometry">
+     <rect>
+      <x>270</x>
+      <y>30</y>
+      <width>75</width>
+      <height>23</height>
+     </rect>
+    </property>
+    <property name="text">
+     <string>Нарисовать</string>
+    </property>
+   </widget>
+  </widget>
+  <widget class="QMenuBar" name="menubar">
+   <property name="geometry">
+    <rect>
+     <x>0</x>
+     <y>0</y>
+     <width>600</width>
+     <height>21</height>
+    </rect>
+   </property>
+  </widget>
+  <widget class="QStatusBar" name="statusbar"/>
+ </widget>
+ <resources/>
+ <connections/>
+</ui>
+'''
+
+
+class Draw(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        f = io.StringIO(template)
+        uic.loadUi(f, self)
+        self.button.clicked.connect(self.paint)
+        self.do_paint = False
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.drow_circle(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def drow_circle(self, qp):
+        qp.setBrush(QColor(choice(range(0, 255)), choice(range(0, 255)), choice(range(0, 255))))
+        r = choice(range(10, 100))
+        qp.drawEllipse(QPointF(300, 300), r, r)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Draw()
+    ex.show()
+    sys.exit(app.exec())
